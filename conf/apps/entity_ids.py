5c5f2263f30390d50e409b430d4b364e2f5f0502
Florian Kempenich <Flori@nKempenich.com>
2018-03-09 08:21:48 +0000

    Finally clean up entity_ids thanks to the EntityRegistery

--- conf/apps/entity_ids.py
+++ conf/apps/entity_ids.py
@@ -1,29 +1,32 @@
 ID = {
     'kitchen': {
-        'motion_sensor': 'binary_sensor.motion_sensor_158d0001660f47',
-        'light': 'light.kitchen_bulb',
-        'speaker': 'media_player.kitchen'
+        'motion_sensor': 'binary_sensor.kitchen_motion',
+        'light': 'light.kitchen_light',
+        'speaker': 'media_player.kitchen_speaker',
+        'temperature': 'sensor.kitchen_temperature'
     },
     'bathroom': {
-        'motion_sensor': 'binary_sensor.motion_sensor_158d0001656bba',
-        'button': 'binary_sensor.switch_158d0001bd1767',
-        'led_light': 'light.gateway_light_7811dcb356c9',
-        'speaker': 'media_player.bathroom',
-        'water_heater': 'switch.water_heater_switch'
+        'motion_sensor': 'binary_sensor.bathroom_motion',
+        'button': 'binary_sensor.bathroom_button',
+        'led_light': 'light.bathroom_gateway_light',
+        'speaker': 'media_player.bathroom_speaker',
+        'water_heater': 'switch.water_heater'
     },
     'living_room': {
-        'motion_sensor': 'binary_sensor.motion_sensor_158d0001b1a392',
-        'soundbar': 'media_player.soundbar',
-        'controller': 'media_player.controller'
+        'motion_sensor': 'binary_sensor.living_room_motion',
+        'soundbar': 'media_player.soundbar_speaker',
+        'controller': 'media_player.controller_speaker',
+        'temperature': 'sensor.living_room_temperature'
     },
     'outside': {
-        'temperature': '??',
-        'button': 'binary_sensor.switch_158d0001bd15b7'
+        'button': 'binary_sensor.washing_machine_button',
+        'temperature': 'sensor.outside_temperature'
     },
     'cast_groups': {
-        'entire_flat': 'media_player.the_entire_flat'
+        'entire_flat': 'media_player.the_entire_flat_speaker_group'
     },
     'debug': {
         'flic_black': 'flic_80e4da71a8b3'
     }
 }
+
