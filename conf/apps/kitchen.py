4af03bd9fb3075105436a40c1c98f86ac1e411ff
Florian Kempenich <Flori@nKempenich.com>
2018-07-01 13:41:54 +0100

    Apply PEP8 formatting

--- conf/apps/kitchen.py
+++ conf/apps/kitchen.py
@@ -7,11 +7,16 @@ try:
 except ModuleNotFoundError:
     from entity_ids import ID
 
+
 class Kitchen(hass.Hass):
     def initialize(self):
-        self.listen_event(self._new_motion, 'motion', entity_id=ID['kitchen']['motion_sensor'])
-        self.listen_state(self._no_more_motion, ID['kitchen']['motion_sensor'], new='off')
+        self.listen_event(self._new_motion, 'motion',
+                          entity_id=ID['kitchen']['motion_sensor'])
+        self.listen_state(self._no_more_motion,
+                          ID['kitchen']['motion_sensor'], new='off')
+
     def _new_motion(self, _event, _data, _kwargs):
         self.turn_on(ID['kitchen']['light'])
+
     def _no_more_motion(self, _entity, _attribute, _old, _new, _kwargs):
         self.turn_off(ID['kitchen']['light'])
