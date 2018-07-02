75f0d64b9f45577d2a8d32331d5a1550ed7c6508
Florian Kempenich <Flori@nKempenich.com>
2018-07-02 14:16:22 +0100

    Implement turn OFF on click

--- conf/apps/kitchen.py
+++ conf/apps/kitchen.py
@@ -7,6 +7,12 @@ try:
 except ModuleNotFoundError:
     from entity_ids import ID
 
+# TODO: Put this in config (through apps.yml, check doc)
+PHONE_PUSHBULLET_ID = "OnePlus 5T"
+
+TURNED_OFF_MSG = "Water Heater was turned OFF"
+TURNED_ON_MSG = "Water Heater was turned back ON"
+
 
 class Kitchen(hass.Hass):
     def initialize(self):
@@ -14,9 +20,16 @@ class Kitchen(hass.Hass):
                           entity_id=ID['kitchen']['motion_sensor'])
         self.listen_state(self._no_more_motion,
                           ID['kitchen']['motion_sensor'], new='off')
+        self.listen_event(self._new_button_click, 'click',
+                          entity_id=ID['kitchen']['button'], click_type='single')
 
     def _new_motion(self, _event, _data, _kwargs):
         self.turn_on(ID['kitchen']['light'])
 
     def _no_more_motion(self, _entity, _attribute, _old, _new, _kwargs):
         self.turn_off(ID['kitchen']['light'])
+
+    def _new_button_click(self, _e, _d, _k):
+        self.turn_off(ID['bathroom']['water_heater'])
+        self.call_service('notify/pushbullet',
+                          target=PHONE_PUSHBULLET_ID, message=TURNED_OFF_MSG)
--- conf/tests/test_kitchen.py
+++ conf/tests/test_kitchen.py
@@ -3,6 +3,8 @@ import pytest
 from mock import patch, MagicMock
 from apps.entity_ids import ID
 
+# TODO: Put this in config (through apps.yml, check doc)
+PHONE_PUSHBULLET_ID="OnePlus 5T"
 
 @pytest.fixture
 def kitchen(given_that):
@@ -12,6 +14,7 @@ def kitchen(given_that):
 
     given_that.mock_functions_are_cleared()
     return kitchen
+
 
 @pytest.fixture
 def when_new(kitchen):
@@ -23,12 +26,62 @@ def when_new(kitchen):
             kitchen._no_more_motion(
                 None,  None, None, None, None)
 
+        def click_button(self):
+            kitchen._new_button_click(None, None, None)
+
     return WhenNewWrapper()
 
-def test_turn_on(when_new, assert_that):
-    when_new.motion()
-    assert_that(ID['kitchen']['light']).was.turned_on()
+class TestInitialization:
+    def test_callbacks_are_registered(self, kitchen, hass_functions):
+        # Given: The mocked callback Appdaemon registration functions
+        listen_event = hass_functions['listen_event']
+        listen_state = hass_functions['listen_state']
 
-def test_turn_off(when_new, assert_that):
-    when_new.no_more_motion()
-    assert_that(ID['kitchen']['light']).was.turned_off()
+        # When: Calling `initialize`
+        kitchen.initialize()
+
+        # Then: callbacks are registered
+        listen_event.assert_any_call(
+            kitchen._new_button_click,
+            'click',
+            entity_id=ID['kitchen']['button'],
+            click_type='single')
+
+        listen_event.assert_any_call(
+            kitchen._new_motion,
+            'motion',
+            entity_id=ID['kitchen']['motion_sensor'])
+
+        listen_state.assert_any_call(
+            kitchen._no_more_motion,
+            ID['kitchen']['motion_sensor'],
+            new='off')
+
+
+class TestAutomaticLights:
+    def test_turn_on(self, when_new, assert_that):
+        when_new.motion()
+        assert_that(ID['kitchen']['light']).was.turned_on()
+
+    def test_turn_off(self, when_new, assert_that):
+        when_new.no_more_motion()
+        assert_that(ID['kitchen']['light']).was.turned_off()
+
+
+class TestClickOnButton:
+    def test_turn_off_water_heater(self, when_new, assert_that):
+        when_new.click_button()
+        assert_that(ID['bathroom']['water_heater']).was.turned_off()
+
+    def test_send_notification(self, when_new, assert_that):  # TODO
+        when_new.click_button()
+        assert_that('notify/pushbullet').was.called_with(
+                message="Water Heater was turned OFF",
+                target="OnePlus 5T")
+
+    class TestAfterDelay:
+        def test_turn_water_heater_back_on(self, when_new, assert_that):  # TODO
+            pass
+
+        def test_send_notification(self, when_new, assert_that):  # TODO
+            pass
