9b80385d1e010e8caa42eb9eebd4e4f82bae8c7c
Florian Kempenich <Flori@nKempenich.com>
2018-07-04 10:10:16 +0100

    Greatly improve time_travel mechanism

--- conf/appdaemon_test_framework/time_travel.py
+++ conf/appdaemon_test_framework/time_travel.py
@@ -1,18 +1,27 @@
 
 class TimeTravelWrapper:
     """
-    Simulate going forward in time
+    Simulate going forward in time.
+
+    It calls all the functions that have been registered with AppDaemon
+    for a later schedule run. A function is only called if it's scheduled
+    time is before or at the simulated time.
+
+    You can chain the calls and call `fast_forward` multiple times in a single test
+
 
     Format:
     > time_travel.fast_forward(10).minutes()
     > # Or
     > time_travel.fast_forward(30).seconds()
 
-    WARNING: You can not chain the calls. Only call `time_travel` once per test
     """
 
     def __init__(self, hass_functions):
-        self.run_in_mock = hass_functions['run_in']
+        self.run_in_mock = RunInMock()
+
+        run_in_magic_mock = hass_functions['run_in']
+        run_in_magic_mock.side_effect = self.run_in_mock
 
     def fast_forward(self, duration):
         time_travel_wrapper = self
@@ -30,16 +39,46 @@ class TimeTravelWrapper:
         self._fast_forward_seconds(minutes * 60)
 
     def _fast_forward_seconds(self, seconds_to_fast_forward):
-        all_registered_callbacks = []
-        for call in self.run_in_mock.call_args_list:
-            args, _ = call
-            callback, delay_in_s = args
-            all_registered_callbacks.append({
-                'callback_function': callback,
-                'delay_in_s': delay_in_s
-            })
+        self.run_in_mock.fast_forward(seconds_to_fast_forward)
 
-            for registered_callback in all_registered_callbacks:
-                if registered_callback['delay_in_s'] <= seconds_to_fast_forward:
-                    kwargs_to_pass_to_callback = {} #TODO: Add support!
-                    registered_callback['callback_function'](kwargs_to_pass_to_callback)
+
+class RunInMock:
+    #  self.run_in(self._after_delay, minutes * 60)
+    def __init__(self):
+        self.all_registered_callbacks = []
+        self._now = 0
+
+    def __call__(self, callback, delay_in_s):
+        self.all_registered_callbacks.append({
+            'callback_function': callback,
+            'delay_in_s': delay_in_s,
+            'registered_at': self._now
+            # TODO: Add support for kwargs
+        })
+
+    def fast_forward(self, seconds):
+        self._now += seconds
+
+        self._run_callbacks()
+
+    def _run_callbacks(self):
+        def _should_run(callback_registration):
+            delay_in_s = callback_registration['delay_in_s']
+            registered_at = callback_registration['registered_at']
+
+            scheduled_time = registered_at + delay_in_s
+            scheduled_now_or_before = scheduled_time <= self._now
+
+            return scheduled_now_or_before
+
+        def _run(callback_registration):
+            kwargs_to_pass_to_callback = {}  # TODO: Add support!
+            registration['callback_function'](kwargs_to_pass_to_callback)
+
+        def _remove(callback_registration):
+            self.all_registered_callbacks.remove(callback_registration)
+
+        for registration in self.all_registered_callbacks:
+            if _should_run(registration):
+                _run(registration)
+                _remove(registration)
