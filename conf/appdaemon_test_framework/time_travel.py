c8e4337bee917787eca45af3f793dec2d987c2c7
Florian Kempenich <Flori@nKempenich.com>
2018-07-04 10:35:13 +0100

    Add mechanism to assert current time

--- conf/appdaemon_test_framework/time_travel.py
+++ conf/appdaemon_test_framework/time_travel.py
@@ -1,20 +1,6 @@
-
 class TimeTravelWrapper:
     """
-    Simulate going forward in time.
-
-    It calls all the functions that have been registered with AppDaemon
-    for a later schedule run. A function is only called if it's scheduled
-    time is before or at the simulated time.
-
-    You can chain the calls and call `fast_forward` multiple times in a single test
-
-
-    Format:
-    > time_travel.fast_forward(10).minutes()
-    > # Or
-    > time_travel.fast_forward(30).seconds()
-
+    AppDaemon Test Framework Utility to simulate going forward in time
     """
 
     def __init__(self, hass_functions):
@@ -24,40 +10,70 @@ class TimeTravelWrapper:
         run_in_magic_mock.side_effect = self.run_in_mock
 
     def fast_forward(self, duration):
-        time_travel_wrapper = self
+        """
+        Simulate going forward in time.
 
-        class UnitsWrapper:
-            def minutes(self):
-                time_travel_wrapper._fast_forward_minutes(duration)
+        It calls all the functions that have been registered with AppDaemon
+        for a later schedule run. A function is only called if it's scheduled
+        time is before or at the simulated time.
 
-            def seconds(self):
-                time_travel_wrapper._fast_forward_seconds(duration)
+        You can chain the calls and call `fast_forward` multiple times in a single test
 
-        return UnitsWrapper()
+        Format:
+        > time_travel.fast_forward(10).minutes()
+        > # Or
+        > time_travel.fast_forward(30).seconds()
+        """
+        return UnitsWrapper(duration, self._fast_forward_seconds)
 
-    def _fast_forward_minutes(self, minutes):
-        self._fast_forward_seconds(minutes * 60)
+    def assert_current_time(self, expected_current_time):
+        """
+        Assert the current time is as expected
+
+        Expected current time is expressed as a duration from T = 0
+
+        Format:
+        > time_travel.assert_current_time(10).minutes()
+        > # Or
+        > time_travel.assert_current_time(30).seconds()
+        """
+        return UnitsWrapper(expected_current_time, self._assert_current_time_seconds)
+
 
     def _fast_forward_seconds(self, seconds_to_fast_forward):
         self.run_in_mock.fast_forward(seconds_to_fast_forward)
 
+    def _assert_current_time_seconds(self, expected_seconds_from_start):
+        assert self.run_in_mock.now == expected_seconds_from_start
+
+
+class UnitsWrapper:
+    def __init__(self, duration, function_with_arg_in_seconds):
+        self.duration = duration
+        self.function_with_arg_in_seconds = function_with_arg_in_seconds
+
+    def minutes(self):
+        self.function_with_arg_in_seconds(self.duration * 60)
+
+    def seconds(self):
+        self.function_with_arg_in_seconds(self.duration)
 
 class RunInMock:
     #  self.run_in(self._after_delay, minutes * 60)
     def __init__(self):
         self.all_registered_callbacks = []
-        self._now = 0
+        self.now = 0
 
     def __call__(self, callback, delay_in_s):
         self.all_registered_callbacks.append({
             'callback_function': callback,
             'delay_in_s': delay_in_s,
-            'registered_at': self._now
+            'registered_at': self.now
             # TODO: Add support for kwargs
         })
 
     def fast_forward(self, seconds):
-        self._now += seconds
+        self.now += seconds
 
         self._run_callbacks()
 
@@ -67,7 +83,7 @@ class RunInMock:
             registered_at = callback_registration['registered_at']
 
             scheduled_time = registered_at + delay_in_s
-            scheduled_now_or_before = scheduled_time <= self._now
+            scheduled_now_or_before = scheduled_time <= self.now
 
             return scheduled_now_or_before
 
