6290f869b28b6f93591ae92ed4bc4405ecf7f781
Florian Kempenich <Flori@nKempenich.com>
2018-07-04 09:13:06 +0100

    Implement 'double-click'

--- conf/apps/kitchen.py
+++ conf/apps/kitchen.py
@@ -10,12 +10,13 @@ except ModuleNotFoundError:
 # TODO: Put this in config (through apps.yml, check doc)
 PHONE_PUSHBULLET_ID = "device/OnePlus 5T"
 SHORT_DELAY = 10
-LONG_DELAY = 10
+LONG_DELAY = 30
 
 MSG_TITLE = "Water Heater"
 MSG_SHORT_OFF = f"was turned off for {SHORT_DELAY} minutes"
-MSG_LONG_OFF =  f"was turned off for {LONG_DELAY} minutes"
+MSG_LONG_OFF = f"was turned off for {LONG_DELAY} minutes"
 MSG_ON = "was turned back on"
+
 
 class Kitchen(hass.Hass):
     def initialize(self):
@@ -25,6 +26,8 @@ class Kitchen(hass.Hass):
                           ID['kitchen']['motion_sensor'], new='off')
         self.listen_event(self._new_button_click, 'click',
                           entity_id=ID['kitchen']['button'], click_type='single')
+        self.listen_event(self._new_button_double_click, 'click',
+                          entity_id=ID['kitchen']['button'], click_type='double')
 
     def _new_motion(self, _event, _data, _kwargs):
         self.turn_on(ID['kitchen']['light'])
@@ -33,16 +36,26 @@ class Kitchen(hass.Hass):
         self.turn_off(ID['kitchen']['light'])
 
     def _new_button_click(self, _e, _d, _k):
-        self.turn_off(ID['bathroom']['water_heater'])
-        self.call_service('notify/pushbullet',
-                          target=PHONE_PUSHBULLET_ID, 
-                          title=MSG_TITLE,
-                          message=MSG_SHORT_OFF)
-        self.run_in(self._after_delay, SHORT_DELAY * 60)
+        self._turn_off_water_heater_for_X_minutes(SHORT_DELAY)
+        self._send_water_heater_notification(MSG_SHORT_OFF)
 
-    def _after_delay(self, _kwargs):
-        self.turn_on(ID['bathroom']['water_heater'])
+    def _new_button_double_click(self, _e, _d, _k):
+        self._turn_off_water_heater_for_X_minutes(LONG_DELAY)
+        self._send_water_heater_notification(MSG_LONG_OFF)
+
+    def _new_button_long_press(self, _e, _d, _k):
+        pass
+
+    def _turn_off_water_heater_for_X_minutes(self, minutes):
+        self.turn_off(ID['bathroom']['water_heater'])
+        self.run_in(self._after_delay, minutes * 60)
+
+    def _send_water_heater_notification(self, message):
         self.call_service('notify/pushbullet',
                           target=PHONE_PUSHBULLET_ID,
                           title=MSG_TITLE,
-                          message=MSG_ON)
+                          message=message)
+
+    def _after_delay(self, _kwargs):
+        self.turn_on(ID['bathroom']['water_heater'])
+        self._send_water_heater_notification(MSG_ON)
--- conf/tests/test_kitchen.py
+++ conf/tests/test_kitchen.py
@@ -27,8 +27,12 @@ def when_new(kitchen):
             kitchen._no_more_motion(
                 None,  None, None, None, None)
 
-        def click_button(self):
-            kitchen._new_button_click(None, None, None)
+        def click_button(self, type='single'):
+            {
+                'single': kitchen._new_button_click,
+                'double': kitchen._new_button_double_click,
+                'long': kitchen._new_button_long_press
+            }[type](None, None, None)
 
     return WhenNewWrapper()
 
@@ -48,6 +52,12 @@ class TestInitialization:
             'click',
             entity_id=ID['kitchen']['button'],
             click_type='single')
+
+        listen_event.assert_any_call(
+            kitchen._new_button_double_click,
+            'click',
+            entity_id=ID['kitchen']['button'],
+            click_type='double')
 
         listen_event.assert_any_call(
             kitchen._new_motion,
@@ -70,8 +80,9 @@ class TestAutomaticLights:
         assert_that(ID['kitchen']['light']).was.turned_off()
 
 
-SHORT_DELAY=10
-LONG_DELAY=30
+SHORT_DELAY = 10
+LONG_DELAY = 30
+
 
 class TestSingleClickOnButton:
     def test_turn_off_water_heater(self, when_new, assert_that):
@@ -94,6 +105,47 @@ class TestSingleClickOnButton:
         def test_send_notification(self, when_new, time_travel, assert_that):
             when_new.click_button()
             time_travel.fast_forward(SHORT_DELAY).minutes()
+            assert_that('notify/pushbullet').was.called_with(
+                title="Water Heater",
+                message="was turned back on",
+                target=PHONE_PUSHBULLET_ID)
+
+
+class TestDoubleClickOnButton:
+    def test_turn_off_water_heater(self, when_new, assert_that):
+        when_new.click_button(type='double')
+        assert_that(ID['bathroom']['water_heater']).was.turned_off()
+
+    def test_send_notification(self, when_new, assert_that):
+        when_new.click_button(type='double')
+        assert_that('notify/pushbullet').was.called_with(
+            title="Water Heater",
+            message=f"was turned off for {LONG_DELAY} minutes",
+            target=PHONE_PUSHBULLET_ID)
+
+    class TestAfterShortDelay:
+        def test_DOES_NOT_turn_water_heater_back_on(self, when_new, time_travel, assert_that):
+            when_new.click_button(type='double')
+            time_travel.fast_forward(SHORT_DELAY).minutes()
+            assert_that(ID['bathroom']['water_heater']).was_not.turned_on()
+
+        def test_DOES_NOT_send_notification(self, when_new, time_travel, assert_that):
+            when_new.click_button(type='double')
+            time_travel.fast_forward(SHORT_DELAY).minutes()
+            assert_that('notify/pushbullet').was_not.called_with(
+                title="Water Heater",
+                message="was turned back on",
+                target=PHONE_PUSHBULLET_ID)
+
+    class TestAfterLongDelay:
+        def test_turn_water_heater_back_on(self, when_new, time_travel, assert_that):
+            when_new.click_button(type='double')
+            time_travel.fast_forward(LONG_DELAY).minutes()
+            assert_that(ID['bathroom']['water_heater']).was.turned_on()
+
+        def test_send_notification(self, when_new, time_travel, assert_that):
+            when_new.click_button(type='double')
+            time_travel.fast_forward(LONG_DELAY).minutes()
             assert_that('notify/pushbullet').was.called_with(
                 title="Water Heater",
                 message="was turned back on",
