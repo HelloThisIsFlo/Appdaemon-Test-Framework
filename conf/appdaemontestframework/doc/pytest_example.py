407fdc244679e2bcc4d7bf802c135e3428009cc2
Florian Kempenich <Flori@nKempenich.com>
2018-07-16 17:14:55 +0200

    Add examples for Test Framework

---
+++ conf/appdaemontestframework/doc/pytest_example.py
@@ -0,0 +1,22 @@
+import pytest
+from appdaemontestframework import patch_hass, GivenThatWrapper, AssertThatWrapper, TimeTravelWrapper
+from myrooms import LivingRoom
+
+@pytest.fixture
+def living_room(given_that):
+    living_room = LivingRoom(None, None, None, None, None, None, None, None)
+    living_room.initialize()
+    given_that.mock_functions_are_cleared()
+    return living_room
+
+
+def test_during_night_light_turn_on(given_that, living_room, assert_that):
+    given_that.state_of('sensor.living_room_illumination').is_set_to(200) # 200lm == night
+    living_room._new_motion(None, None, None)
+    assert_that('light.living_room').was.turned_on()
+
+def test_during_day_light_DOES_NOT_turn_on(given_that, living_room, assert_that):
+    given_that.state_of('sensor.living_room_illumination').is_set_to(1000) # 1000lm == sunlight
+    living_room._new_motion(None, None, None)
+    assert_that('light.living_room').was_not.turned_on()
+
---
+++ conf/appdaemontestframework/doc/unittest_example.py
@@ -0,0 +1,35 @@
+import unittest
+from appdaemontestframework import patch_hass, GivenThatWrapper, AssertThatWrapper, TimeTravelWrapper
+from myrooms import LivingRoom
+
+class AppdaemonTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        patched_hass_functions, unpatch_callback = patch_hass()
+
+        cls.given_that = GivenThatWrapper(patched_hass_functions)
+        cls.assert_that = AssertThatWrapper(patched_hass_functions)
+        cls.time_travel = TimeTravelWrapper(patched_hass_functions)
+
+        cls.unpatch_callback = unpatch_callback
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.unpatch_callback()
+
+
+class LivingRoomTestCase(AppdaemonTestCase):
+    def setUp(self):
+        self.living_room = LivingRoom(None, None, None, None, None, None, None, None)
+        self.living_room.initialize()
+        self.given_that.mock_functions_are_cleared()
+
+    def test_during_night_light_turn_on(self):
+        self.given_that.state_of('sensor.living_room_illumination').is_set_to(200) # 200lm == night
+        self.living_room._new_motion(None, None, None)
+        self.assert_that('light.living_room').was.turned_on()
+
+    def test_during_day_light_DOES_NOT_turn_on(self):
+        self.given_that.state_of('sensor.living_room_illumination').is_set_to(1000) # 1000lm == sun light
+        self.living_room._new_motion(None, None, None)
+        self.assert_that('light.living_room').was_not.turned_on()
