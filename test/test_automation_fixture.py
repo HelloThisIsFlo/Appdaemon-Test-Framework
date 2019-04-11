d4ab5e254796fb71e27dff5105b577a25ab3cf14
Florian Kempenich <Flori@nKempenich.com>
2019-04-11 11:03:58 +0100

    Move pytester config fixture to conftest

--- test/test_automation_fixture.py
+++ test/test_automation_fixture.py
@@ -23,39 +23,8 @@ def expected_error_regex_was_found_in_stdout_lines(result, expected_error_regex)
     return False
 
 
-@fixture
-def configure_appdaemontestframework(testdir):
-    testdir.makeconftest(
-        """
-        import pytest
-        from appdaemontestframework import patch_hass, AssertThatWrapper, GivenThatWrapper, TimeTravelWrapper
-        
-        
-        @pytest.fixture
-        def hass_functions():
-            patched_hass_functions, unpatch_callback = patch_hass()
-            yield patched_hass_functions
-            unpatch_callback()
-        
-        
-        @pytest.fixture
-        def given_that(hass_functions):
-            return GivenThatWrapper(hass_functions)
-        
-        
-        @pytest.fixture
-        def assert_that(hass_functions):
-            return AssertThatWrapper(hass_functions)
-        
-        
-        @pytest.fixture
-        def time_travel(hass_functions):
-            return TimeTravelWrapper(hass_functions)
-    """)
-
-
 @mark.using_pytester
-@mark.usefixtures('configure_appdaemontestframework')
+@mark.usefixtures('configure_appdaemontestframework_for_pytester')
 class TestAutomationFixture:
     def test_fixture_is_available_for_injection(self, testdir):
         testdir.makepyfile(
