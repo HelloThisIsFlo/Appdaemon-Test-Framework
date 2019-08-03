9247e8ad6219028bdd59aa0d437390a240c9743f
Florian Kempenich <Flori@nKempenich.com>
2019-08-03 13:09:00 +0100

    Release 2.3.2: Patch 'notify'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='appdaemontestframework',
-    version='2.3.1',
+    version='2.3.2',
     description='Clean, human-readable tests for Appdaemon',
     long_description='See: https://floriankempenich.github.io/Appdaemon-Test-Framework/',
     keywords='appdaemon homeassistant test tdd clean-code home-automation',
