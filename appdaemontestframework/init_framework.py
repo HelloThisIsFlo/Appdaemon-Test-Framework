52b31817a81641f332dc8a3195c7ab95ee113bfa
Matt Campbell <calvinmc@gmail.com>
2019-09-22 15:26:24 -0400

    alsmost all tests going after refactor of mock init

--- appdaemontestframework/init_framework.py
+++ appdaemontestframework/init_framework.py
@@ -3,81 +3,83 @@ import logging
 import mock
 from appdaemon.plugins.hass.hassapi import Hass
 
+class MockHandler:
+    def __init__(self, object_to_patch, function_name, side_effect=None):
+        self.function_name = function_name
+        self.patch = mock.patch.object(object_to_patch, self.function_name, create=True)
+        self.mock = self.patch.start()
+        self.mock.return_value = None
+        self.mock.side_effect = side_effect
+
 
 class HassMock:
     def __init__(self):
-        actionable_functions_to_patch = [
+        self._mocks = [
             # Meta
-            '__init__',  # Patch the __init__ method to skip Hass initialisation
+            MockHandler(Hass, '__init__'),  # Patch the __init__ method to skip Hass initialization
 
             # Logging
-            'log',
-            'error',
+            MockHandler(Hass, 'log'),
+            MockHandler(Hass, 'error'),
 
             # Scheduler callback registrations functions
-            'run_in',
-            'run_once',
-            'run_at',
-            'run_daily',
-            'run_hourly',
-            'run_minutely',
-            'run_every',
-            'cancel_timer',
+            MockHandler(Hass, 'run_in'),
+            MockHandler(Hass, 'run_once'),
+            MockHandler(Hass, 'run_at'),
+            MockHandler(Hass, 'run_daily'),
+            MockHandler(Hass, 'run_hourly'),
+            MockHandler(Hass, 'run_minutely'),
+            MockHandler(Hass, 'run_every'),
+            MockHandler(Hass, 'cancel_timer'),
 
             # Sunrise and sunset functions
-            'run_at_sunrise',
-            'run_at_sunset',
+            MockHandler(Hass, 'run_at_sunrise'),
+            MockHandler(Hass, 'run_at_sunset'),
 
             # Listener callback registrations functions
-            'listen_event',
-            'listen_state',
+            MockHandler(Hass, 'listen_event'),
+            MockHandler(Hass, 'listen_state'),
 
             # State functions / attr
-            'set_state',
-            'get_state',
-            'time',
-            'args',  # Not a function, attribute. But same patching logic
+            MockHandler(Hass, 'set_state'),
+            MockHandler(Hass, 'get_state'),
+            MockHandler(Hass, 'time'),
+            MockHandler(Hass, 'args'),  # Not a function, attribute. But same patching logic
 
             # Interactions functions
-            'call_service',
-            'turn_on',
-            'turn_off',
+            MockHandler(Hass, 'call_service'),
+            MockHandler(Hass, 'turn_on'),
+            MockHandler(Hass, 'turn_off'),
 
             # Custom callback functions
-            'register_constraint',
-            'now_is_between',
-            'notify'
+            MockHandler(Hass, 'register_constraint'),
+            MockHandler(Hass, 'now_is_between'),
+            MockHandler(Hass, 'notify'),
+
+            # logging
+            MockHandler(Hass, 'log', side_effect=self._log_log),
+            MockHandler(Hass, 'error', side_effect=self._log_error),
         ]
 
-        self._patches = []
+        # TODO: remove this temp convert the new _mocks into the old _hass_functions
         self._hass_functions = {}
-        for function_name in actionable_functions_to_patch:
-            patch_function = mock.patch.object(Hass, function_name, create=True)
-            self._patches.append(patch_function)
-            patched_function = patch_function.start()
-            patched_function.return_value = None
-            self._hass_functions[function_name] = patched_function
-
-        self._ensure_compatibility_with_previous_versions()
-        self._mock_logging()
-
-    def _ensure_compatibility_with_previous_versions(self):
+        for mock_handler in self._mocks:
+            self._hass_functions[mock_handler.function_name] = mock_handler.mock
+        # ensure compatibility with older versions of AppDaemon
         self._hass_functions['passed_args'] = self._hass_functions['args']
 
+    def unpatch_mocks(self):
+        for mock_handler in self._mocks:
+            mock_handler.mock.stop()
 
-    def _mock_logging(self):
+
+    ### Logging mocks
+    @staticmethod
+    def _log_error(msg, level='ERROR'):
+        log_log(msg, level)
+
+    @staticmethod
+    def _log_log(msg, level='INFO'):
         # Renamed the function to remove confusion
         get_logging_level_from_name = logging.getLevelName
-
-        def log_error(msg, level='ERROR'):
-            log_log(msg, level)
-
-        def log_log(msg, level='INFO'):
-            logging.log(get_logging_level_from_name(level), msg)
-
-        self._hass_functions['error'].side_effect = log_error
-        self._hass_functions['log'].side_effect = log_log
-
-    def unpatch_mocks(self):
-        for patch in self._patches:
-            patch.stop()
+        logging.log(get_logging_level_from_name(level), msg)
