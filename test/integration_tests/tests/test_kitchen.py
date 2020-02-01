3a29acf1d258f12c51f4cf0f243621b15ec10cd5
Matt Campbell <calvinmc@gmail.com>
2020-02-01 10:58:32 -0500

    fix deprecated code usage

--- test/integration_tests/tests/test_kitchen.py
+++ test/integration_tests/tests/test_kitchen.py
@@ -35,10 +35,10 @@ def when_new(kitchen):
 
 
 class TestInitialization:
-    def test_callbacks_are_registered(self, kitchen, hass_functions):
+    def test_callbacks_are_registered(self, kitchen, hass_mocks):
         # Given: The mocked callback Appdaemon registration functions
-        listen_event = hass_functions['listen_event']
-        listen_state = hass_functions['listen_state']
+        listen_event = hass_mocks.hass_functions['listen_event']
+        listen_state = hass_mocks.hass_functions['listen_state']
 
         # When: Calling `initialize`
         kitchen.initialize()
