3992e1e00f07f03b46c7e35ab58c67525f17c41f
Florian Kempenich <Flori@nKempenich.com>
2020-03-03 14:58:43 +0000

    Reformat file

--- test/test_state.py
+++ test/test_state.py
@@ -36,7 +36,8 @@ def automation():
     pass
 
 
-def test_state_was_never_set__raise_error(given_that, automation: MockAutomation):
+def test_state_was_never_set__raise_error(given_that,
+                                          automation: MockAutomation):
     with raises(StateNotSetError, match=r'.*State.*was never set.*'):
         automation.get_light_brightness()
 
@@ -49,7 +50,8 @@ def test_set_and_get_state(given_that, automation: MockAutomation):
     assert automation.is_light_turned_on()
 
 
-def test_attribute_was_never_set__raise_error(given_that, automation: MockAutomation):
+def test_attribute_was_never_set__raise_error(given_that,
+                                              automation: MockAutomation):
     given_that.state_of(LIGHT).is_set_to('on')
     assert automation.get_light_brightness() is None
 
@@ -63,31 +65,46 @@ def test_set_and_get_attribute(given_that, automation: MockAutomation):
 
 
 def test_set_and_get_all_attribute(given_that, automation: MockAutomation):
-    given_that.state_of(LIGHT).is_set_to('on', attributes={'brightness': 11, 'color': 'blue'})
+    given_that.state_of(LIGHT).is_set_to('on', attributes={'brightness': 11,
+                                                           'color': 'blue'})
     assert automation.get_all_attributes_from_light() == {
-        'state': 'on', 'last_updated': None, 'last_changed': None, 'entity_id': LIGHT,
+        'state': 'on', 'last_updated': None, 'last_changed': None,
+        'entity_id': LIGHT,
         'attributes': {'brightness': 11, 'color': 'blue'}
-        }
+    }
 
-def test_set_and_get_all_attribute_last_updated_changed(given_that, automation: MockAutomation):
+
+def test_set_and_get_all_attribute_last_updated_changed(given_that,
+                                                        automation: MockAutomation):
     dt = datetime.datetime(year=2000, month=1, day=1).isoformat()
-    given_that.state_of(LIGHT).is_set_to('on', attributes={'brightness': 11, 'color': 'blue', 'last_updated': dt, 'last_changed': dt})
+    given_that.state_of(LIGHT).is_set_to('on', attributes={'brightness': 11,
+                                                           'color': 'blue',
+                                                           'last_updated': dt,
+                                                           'last_changed': dt})
     assert automation.get_all_attributes_from_light() == {
-        'state': 'on', 'last_updated': dt, 'last_changed': dt, 'entity_id': LIGHT,
+        'state': 'on', 'last_updated': dt, 'last_changed': dt,
+        'entity_id': LIGHT,
         'attributes': {'brightness': 11, 'color': 'blue'}
-        }
+    }
+
 
 def test_get_complete_state_dictionary(given_that, automation: MockAutomation):
-    given_that.state_of(LIGHT).is_set_to('on', attributes={'brightness': 11, 'color': 'blue'})
-    given_that.state_of(COVER).is_set_to("closed", {'friendly_name': f"{COVER}", 'current_position': 0})
-    assert automation.get_complete_state_dictionary() == {COVER: {'attributes': {'current_position': 0,
-                                                                                                'friendly_name': COVER},
-                                                                                'state': 'closed'},
-                                                          LIGHT: {'attributes': {'brightness': 11, 'color': 'blue'},
-                                                                                'state': 'on'}}
+    given_that.state_of(LIGHT).is_set_to('on', attributes={'brightness': 11,
+                                                           'color': 'blue'})
+    given_that.state_of(COVER).is_set_to("closed", {'friendly_name': f"{COVER}",
+                                                    'current_position': 0})
+    assert automation.get_complete_state_dictionary() == {
+        COVER: {'attributes': {'current_position': 0,
+                               'friendly_name': COVER},
+                'state': 'closed'},
+        LIGHT: {'attributes': {'brightness': 11, 'color': 'blue'},
+                'state': 'on'}}
+
 
 @pytest.mark.only
-def test_throw_typeerror_when_attributes_arg_not_passed_via_keyword(given_that, automation: MockAutomation):
-    given_that.state_of(LIGHT).is_set_to('on', attributes={'brightness': 11, 'color': 'blue'})
+def test_throw_typeerror_when_attributes_arg_not_passed_via_keyword(given_that,
+                                                                    automation: MockAutomation):
+    given_that.state_of(LIGHT).is_set_to('on', attributes={'brightness': 11,
+                                                           'color': 'blue'})
     with pytest.raises(TypeError):
         automation.get_without_using_keyword()
